3d303a5860d34fe0c0aad0b86446b39790802a0c
The_Pr0fessor <Trendonrobinson@gmail.com>
2022-01-30 13:30:35 -0500

    particles

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,6 +3,8 @@ import './HomeFix.css';
 import { useEffect } from 'react';
 import { useLocation } from "react-router-dom";
 
+import Particles from "react-tsparticles";
+
 function Home() {
 
     let location = useLocation();
@@ -12,8 +14,95 @@ function Home() {
         // Home.classList.add("active"); test
     }, [])
 
+    const particlesInit = (main) => {
+        console.log(main);
+    
+        // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+      };
+    
+      const particlesLoaded = (container) => {
+        console.log(container);
+      };
+
     return (
         <div className="Home">
+            <Particles
+                id="tsparticles"
+                init={particlesInit}
+                loaded={particlesLoaded}
+                options={{
+                    fpsLimit: 30,
+                    interactivity: {
+                    events: {
+                        onClick: {
+                        enable: true,
+                        mode: "push",
+                        },
+                        onHover: {
+                        enable: true,
+                        mode: "attract",
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        bubble: {
+                        distance: 400,
+                        duration: 2,
+                        opacity: 0.8,
+                        size: 40,
+                        },
+                        push: {
+                        quantity: 4,
+                        },
+                        repulse: {
+                        distance: 200,
+                        duration: 0.4,
+                        },
+                    },
+                    },
+                    particles: {
+                    color: {
+                        value: "#ffffff",
+                    },
+                    links: {
+                        color: "#ffffff",
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.5,
+                        width: 1,
+                    },
+                    collisions: {
+                        enable: true,
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outMode: "bounce",
+                        random: false,
+                        speed: 2,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                        enable: true,
+                        area: 800,
+                        },
+                        value: 40,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        random: true,
+                        value: 5,
+                    },
+                    },
+                    detectRetina: true,
+                }}
+                />
             <div className="grid">
                 <div id='title' className='title'>
                     <div className='name'>
