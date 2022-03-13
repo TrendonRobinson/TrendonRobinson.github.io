67f1b58afd5a2be6761b2f3007c99dca2cb5661f
The_Pr0fessor <Trendonrobinson@gmail.com>
2022-03-13 19:28:49 -0400

    slides

--- src/Components/Works.js
+++ src/Components/Works.js
@@ -1,51 +1,120 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import styled from "styled-components";
 
-export default function Works() {
-    return (
-        <WorkContainer>
-            Works
-            <div class="grid grid-cols-4 gap-x-4 gap-y-1 max-w-1xl">
-                {/* <!-- Title --> */}
-                <div class="col-span-full mb-3">
-                    <p class="text-xl text-gray-800">
-                        {" "}
-                        Today's popular searches{" "}
-                    </p>
-                </div>
+// Components
+import Nav from "./Nav";
 
-                {/* <!-- Card 1 --> */}
-                <div class="col-span-2">
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+// import "./styles.css";
+
+// import required modules
+import { EffectCoverflow, Pagination } from "swiper";
+
+const projectsInfo = [
+    [
+        'Quirkymals', 
+        'https://tr.rbxcdn.com/b23f9987561066b3b92c7c77ed9fb413/768/432/Image/Png'
+    ],
+    [
+        'Quirkymals', 
+        'https://i.ibb.co/fdKbG9j/Screen-Shot-2022-03-11-at-4-58-28-AM.png'
+    ],
+    [
+        'Quirkymals', 
+        'https://tr.rbxcdn.com/b23f9987561066b3b92c7c77ed9fb413/768/432/Image/Png'
+    ],
+    [
+        'Quirkymals', 
+        'https://tr.rbxcdn.com/b23f9987561066b3b92c7c77ed9fb413/768/432/Image/Png'
+    ],
+    
+    
+];
+
+export default function Works(props) {
+    const { classes } = props;
+
+    const pagination = {
+        clickable: true,
+        renderBullet: function (index, className) {
+            return (
+                '<button class="' + className + '">' + (index + 1) + "</button>"
+            );
+        },
+    };
+
+    const projects = projectsInfo.map((element, index) => {
+        return (
+            <SwiperSlide>
+                <Content class="col-span-2">
                     <a href="link">
                         <img
-                            src="https://i.ibb.co/fdKbG9j/Screen-Shot-2022-03-11-at-4-58-28-AM.png"
+                            src={element[1]}//
                             class="rounded-xl brightness-75"
                             alt="name"
                         />
                     </a>
-                    <p class="text-xs -translate-y-6 text-white font-semibold sm:-translate-y-8 sm:text-base translate-x-3">
-                        Project Name
-                    </p>
-                </div>
-                {/* <!-- Card 1 --> */}
-                <div class="col-span-2">
-                    <a href="link">
-                        <img
-                            src="https://i.ibb.co/fdKbG9j/Screen-Shot-2022-03-11-at-4-58-28-AM.png"
-                            class="rounded-xl brightness-75"
-                            alt="name"
-                        />
-                    </a>
-                    <p class="text-xs -translate-y-6 text-white font-semibold sm:-translate-y-8 sm:text-base translate-x-3">
-                        Project Name
-                    </p>
-                </div>
-            </div>
+                    <Description>
+                        <h1 class="title">{element[0]}</h1>
+                    </Description>
+                </Content>
+            </SwiperSlide>
+        );
+    });
+
+    return (
+        <WorkContainer className={classes}>
+            <Swiper
+                pagination={pagination}
+                spaceBetween={50}
+                modules={[Pagination]}
+                slidesPerView={1}
+                onSlideChange={() => console.log("slide change")}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+                {projects}
+            </Swiper>
         </WorkContainer>
     );
 }
 
-
 const WorkContainer = styled.div`
-    padding-top: 20%;
-`
+    width: 100vw;
+    height: 100vh;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    /* flex-direction: row; */
+`;
+
+const Content = styled.div`
+    /* display: flex;
+    flex-direction: row; */
+
+    img {
+        width: 600px;
+        height: auto;
+    }
+
+
+    h5 {
+        font-size: 10px
+    }
+`;
+
+const Description = styled.div`
+    position: absolute;
+    transform: translate(-30%, -225%);
+    font-weight: 700;
+    font-size: 70px;
+    /* margin: 0px 5%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    max-width: 200px; */
+`;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,150 +1,77 @@
 import { style } from "@mui/system";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-
-// Components
-import Nav from "../Components/Nav";
+import "../App.css"
 
 // Sections
 import Works from "../Components/Works";
 
 export default function Home() {
+    const [display, setDisplay] = useState(false)
+    const [works, setWorks] = useState(false)
+
+    useEffect(() => {
+        if (display) {
+            setTimeout(() => {
+                setWorks(true)
+            }, 1000/1.5);
+        }
+    }, [display])
+    
+
+    function Display() {
+        console.log('test')
+        setDisplay(true)
+    }
+
     return (
         <Container className="Home">
-            <IntroContainer>
-                <NameContainer>
-                    <Nav />
-                    <Image>
-                        <img
-                            src="https://images.ctfassets.net/fevtq3bap7tj/4rsL8Wiv2EuyaOKgwC0OOU/93dc05128009c2ec38cd34e35dd46fb2/Home_illustration.png"
-                            alt="graphic"
-                        />
-                    </Image>
-                    <LargeText>
-                        Trendon
-                        <br />
-                        Robinson
-                    </LargeText>
-                </NameContainer>
-                <BioContainer>
-                    <Name>TRENDON/R.</Name>
-                    <LightText>{"/THIS IS ME"}</LightText>
-                    <Bio>Experience designer and developer from Georgia.</Bio>
-
-                    <LightText>
-                        {
-                            "A profession software developer. Loves to add clarity to complicated concepts by design."
-                        }
-                    </LightText>
-                    {/* <CodeText style={{fontSize: "200%", paddingTop: "30%"}}>
-                        {`<div>`} 
-                        <IndentedText>{`<h1>Welcome<h1/>`}</IndentedText>
-                        {`<div/>`}
-                    </CodeText> */}
-                </BioContainer>
-            </IntroContainer>
-            <hr style={{width: '100vw', transform: `translate(-14vw, 0px)` }}/>
-            <Works />
+            <GetStarted onClick={Display} className={`Started ${display ? 'hidden' : ''} ${works ? 'hide' : ''}`}>
+                <Name>Trendon Robinson</Name>
+                <StartText>Click to get Started</StartText>
+            </GetStarted>
+            <Works classes={`${works ? 'show' : 'hide'}`}/>
         </Container>
     );
 }
 
-const IntroContainer = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 0.6fr;
-    /* height: 100vh; */
-    @media (max-width: 767.98px) {
-        display: flex;
-        justify-content: center;
-        flex-direction: column;
-    }
-`;
-
-const Container = styled.div``;
-
-const Name = styled.button`
-    justify-self: left;
-    padding-right: 50px;
-
-    &:active {
-    }
-
-    @media (max-width: 767.98px) {
-        display: None;
-    }
-`;
-
-const NameContainer = styled.div`
-    /* width: 300px */
-`;
-const Image = styled.div`
-    @media (min-width: 767.98px) {
-        width: 30vw;
-        min-width: 300px;
-        position: absolute;
-        left: 35vw;
-        top: 35%;
-        transform: translate(-15vw, -50%);
-        z-index: -1;
-    }
-    @media (max-width: 767.98px) {
-    }
-
-    /* width: 300px */
-`;
-
-const ImageTwo = styled.div`
-    @media (min-width: 767.98px) {
-        width: 23vw;
-        position: absolute;
-        left: 67%;
-        top: 63%;
-        transform: translate(-50%, -50%);
-        z-index: -1;
-    }
-    @media (max-width: 767.98px) {
-    }
-
-    /* width: 300px */
-`;
-
-const BioContainer = styled.div`
-    /* width: 300px */
-`;
-const Bio = styled.div`
-    font-size: 30px;
-`;
-
-const LightText = styled.div`
-    opacity: 0.5;
-    font-size: 12px;
-    padding: 50px 0px;
-
-    @media (max-width: 767.98px) {
-        display: None;
-    }
-`;
-
-const CodeText = styled.div`
-    font-family: "Brush Script MT", cursive;
-    opacity: .2;
-
-    @media (max-width: 767.98px){
-    	display: none;
-    }
-`;
-
-const IndentedText = styled.div`
-    text-indent: 20px;
-`;
-
-const LargeText = styled.div`
+const Container = styled.div`
     text-align: center;
-    font-size: 13vw;
-    line-height: 100%;
-    padding-top: 20%;
-    @media (min-width: 767.98px) {
-        padding-top: 27rem;
-        font-size: 8vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    font-size: 5vw;
+    @media (max-width: 767.98px){
+    	font-size: 10vw
+    }
+`;
+
+const Name = styled.div`
+    line-height: normal;
+    font-weight: 100;
+    font-family: "Brush Script MT", 'Brush Script Std', cursive;
+    @media (min-width: 576px){
+    	font-size: 7vw
+    }
+`;
+
+const GetStarted = styled.button`
+    height: 100vh;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`;
+
+const StartText = styled.p`
+    
+    @media (max-width: 767.98px){
+    	font-size: 10px;
+    }
+    @media (min-width: 576px){
+    	font-size: 25px;
     }
 `;
